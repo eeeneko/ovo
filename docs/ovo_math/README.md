2aef339689910c3e7ed34df68709eb892e09d210
IoTgod <i@IoTgod.me>
2019-03-03 20:28:45 +0800

    add md5

---
+++ docs/ovo_math/README.md
@@ -0,0 +1,48 @@
+## Class ovo::math
+### math.md5()
+This function can generate a Hash md5 value from a string.
+#### Params:
+ - **string value** The content need to encode.
+#### Return:
+ - **string Hex md5 code** The generated Hex md5 code.
+#### Example
+````C++
+#include <iostream>
+#include <string>
+#include "ovo.h"
+  
+int main()
+{
+    std::string s = "a";
+    ovo::math m;
+
+    std::cout << m.md5(s) << std::endl;
+    return 0;
+}
+````
+This will get the following result.
+>0cc175b9c0f1b6a831c399e269772661
+#### Advanced
+ - **math.toStr(int length)** You can use this to define the length of the generated code.
+ - **math.getDigest()** Use this generate a binary md5 code.
+#### Advanced Example
+````C++
+#include <iostream>
+#include <string>
+#include "ovo.h"
+  
+int main()
+{
+    std::string s = "a";
+    ovo::math m;
+    m.md5(s);
+
+    std::cout << m.toStr(16) << std::endl;
+    std::cout << m.getDigest() << std::endl;
+    return 0;
+}
+````
+This will get the following result.
+>0cc175b9c0f1b6a8<br/>
+>♀羥估穸?脵鈏w&a
+
--- src/ovo.h
+++ src/ovo.h
@@ -15,6 +15,79 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstring>
+
+
+/* Parameters of md5. */
+#define s11 7
+#define s12 12
+#define s13 17
+#define s14 22
+#define s21 5
+#define s22 9
+#define s23 14
+#define s24 20
+#define s31 4
+#define s32 11
+#define s33 16
+#define s34 23
+#define s41 6
+#define s42 10
+#define s43 15
+#define s44 21
+
+/**
+ * @Basic ovo::math::md5 functions.
+ *
+ * @param there bit32.
+ *
+ * @return one bit32.
+ */
+#define F(x, y, z) (((x) & (y)) | ((~x) & (z)))
+#define G(x, y, z) (((x) & (z)) | ((y) & (~z)))
+#define H(x, y, z) ((x) ^ (y) ^ (z))
+#define I(x, y, z) ((y) ^ ((x) | (~z)))
+
+/**
+ * @Rotate Left.
+ *
+ * @param {num} the raw number.
+ *
+ * @param {n} rotate left n.
+ *
+ * @return the number after rotated left.
+ */
+#define ROTATELEFT(num, n) (((num) << (n)) | ((num) >> (32-(n))))
+
+/**
+ * @Transformations for rounds 1, 2, 3, and 4.
+ */
+#define FF(a, b, c, d, x, s, ac) { \
+  (a) += F ((b), (c), (d)) + (x) + ac; \
+  (a) = ROTATELEFT ((a), (s)); \
+  (a) += (b); \
+}
+#define GG(a, b, c, d, x, s, ac) { \
+  (a) += G ((b), (c), (d)) + (x) + ac; \
+  (a) = ROTATELEFT ((a), (s)); \
+  (a) += (b); \
+}
+#define HH(a, b, c, d, x, s, ac) { \
+  (a) += H ((b), (c), (d)) + (x) + ac; \
+  (a) = ROTATELEFT ((a), (s)); \
+  (a) += (b); \
+}
+#define II(a, b, c, d, x, s, ac) { \
+  (a) += I ((b), (c), (d)) + (x) + ac; \
+  (a) = ROTATELEFT ((a), (s)); \
+  (a) += (b); \
+}
+
+/* Define of btye.*/
+typedef unsigned char byte;
+/* Define of byte. */
+typedef unsigned int bit32;
+
 
 using namespace std;
 
@@ -46,18 +119,60 @@ namespace ovo{
     class file{
 
         public:
-            vector<string> name;
-            vector<unsigned int> size;
-            vector<time_t> time_create;
-            vector<time_t> time_access;
-            vector<time_t> time_write;
-            vector<unsigned int> attrib;
-            inline int num(){return name.size();}
+            /*** Files ***/
+            vector<string> name; //File name
+            vector<unsigned int> size; //File size
+            vector<time_t> time_create; //File created time
+            vector<time_t> time_access; //File last accessed time
+            vector<time_t> time_write; //File last write time
+            vector<unsigned int> attrib; //File attribute
+            inline int num(){return name.size();} //Files numbers
+            /* Get info of Files */
             void get_files_info(const string path, const string format = "*", const int isSearchSubfolders = 0, const int isShowPath = 0);
 
         private:
+            /*** Files ***/
             void get_all_files_info(string path, const string format = "*", const int isShowPath = 0);
             void get_all_folders_name(string path, vector<string>& folders);
+    };
+
+
+    class math {
+
+        public:
+            /*** md5 ***/
+            /* Construct a md5 object with a string. */
+            string md5(const string& message);
+            /* Generate md5 digest. */
+            const byte* getDigest();
+            /* Convert digest to string value */
+            string toStr(const int length = 32);
+
+        private:
+            /*** md5 ***/
+            /* Initialization the md5 object, processing another message block,
+             * and updating the context.*/
+            void init(const byte* input, size_t len);
+            /* md5 basic transformation. Transforms state based on block. */
+            void transform(const byte block[64]);
+            /* Encodes input (usigned long) into output (byte). */
+            void encode(const bit32* input, byte* output, size_t length);
+            /* Decodes input (byte) into output (usigned long). */
+            void decode(const byte* input, bit32* output, size_t length);
+            /* Flag for mark whether calculate finished. */
+            bool finished;
+            /* state (ABCD). */
+            bit32 state[4];
+            /* number of bits, low-order word first. */
+            bit32 count[2];
+            /* input buffer. */
+            byte buffer[64];
+            /* message digest. */
+            byte digest[16];
+            /* padding for calculate. */
+            static const byte PADDING[64];
+            /* Hex numbers. */
+            static const char HEX_NUMBERS[16];
     };
 }
 
